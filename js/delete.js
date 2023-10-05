53a4607c8ff8a212fc87eb06de3358cc0e17192d
Rosalie <rosalie.boulard@gmail.com>
2023-10-05 18:28:40 +0200

    tried but in vain

--- js/delete.js
+++ js/delete.js
@@ -1,12 +1,24 @@
-const deleteBtn = document.querySelectorAll('.delete');
 
-deleteBtn.forEach((bouton) => {
-    bouton.addEventListener("click", function () {
-        let parent = bouton.parentNode.parentNode.parentNode.parentNode.parentNode;
-        parent.remove();
-    })
-});
+
+// deleteBtn.forEach((bouton) => {
+//     bouton.addEventListener("click", function () {
+//         let parent = bouton.parentNode.parentNode.parentNode.parentNode.parentNode;
+//         parent.remove();
+//     })
+// });
 // deleteBtn.addEventListener("click", function(event) {
 //     let parent = deleteBtn.parentNode.parentNode.parentNode.parentNode;
 //     parent.remove();
 // }); 
+
+
+
+function Delete() { 
+    const tasksSection=document.querySelector('.cards');
+    tasksSection.addEventListener('click', function (e) {
+        if (e.target.classList.contains('delete')) {
+            const parent=e.target.closest('.tasks');
+            parent.remove();
+        }
+    })
+}
